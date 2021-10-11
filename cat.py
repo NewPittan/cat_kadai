163e87cdd83e715d17d6ae786cc4b3afaac8df3c
NewPittan <91584923+NewPittan@users.noreply.github.com>
2021-10-11 23:41:23 +0900

    Update cat.py

--- cat.py
+++ cat.py
@@ -2,8 +2,8 @@ import os
 import sys
 args = sys.argv
 os.chdir('..')  # 相対パス
-os.chdir('/'+args[1]+'/'+args[2]+'/'+args[3])  # 絶対パス
-f = open(args[4], 'r', encoding='UTF-8')
+os.chdir(args[1])  # 絶対パス
+f = open(args[2], 'r', encoding='UTF-8')
 
 data = f.read()
 print(data)
